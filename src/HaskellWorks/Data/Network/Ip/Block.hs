00da2bd00b881fa5f2dfaaca02a43972e31e1002
David Turnbull <david@broodax.net>
2018-12-12 15:14:19 +1100

    MAYH-10465 sum type

--- src/HaskellWorks/Data/Network/Ip/Block.hs
+++ src/HaskellWorks/Data/Network/Ip/Block.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE DuplicateRecordFields      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE InstanceSigs               #-}
 {-# LANGUAGE OverloadedStrings          #-}
 
@@ -29,36 +28,34 @@ import qualified HaskellWorks.Data.Network.Ip.Ipv4     as I4
 import qualified HaskellWorks.Data.Network.Ip.Ipv6     as I6
 import qualified Text.ParserCombinators.ReadPrec       as RP
 
-newtype IpBlock = IpBlock
-  { block :: Either I4.Ipv4Block I6.Ipv6Block
-  } deriving (Eq, Ord, Generic)
-
+data IpBlock = IpBlockV4 I4.Ipv4Block | IpBlockV6 I6.Ipv6Block
+  deriving (Eq, Ord, Generic)
 
 instance Show IpBlock where
-  showsPrec _ (IpBlock (Left ipv4Block))  = shows ipv4Block
-  showsPrec _ (IpBlock (Right ipv6Block)) = shows ipv6Block
+  showsPrec _ (IpBlockV4 a) = shows a
+  showsPrec _ (IpBlockV6 a) = shows a
 
 instance Read IpBlock where
   readsPrec _ s =
     case readMaybe s :: Maybe I4.Ipv4Block of
-      Just ipv4 -> [(IpBlock (Left ipv4), "")]
+      Just ipv4 -> [(IpBlockV4 ipv4, "")]
 
       Nothing ->
         case readMaybe s :: Maybe I6.Ipv6Block of
-          Just ipv6 -> [(IpBlock (Right ipv6), "")]
+          Just ipv6 -> [(IpBlockV6 ipv6, "")]
           Nothing   -> []
 
 isValidIpBlock :: IpBlock -> Bool
-isValidIpBlock (IpBlock (Left _))    = True -- TODO
-isValidIpBlock (IpBlock (Right i6b)) = I6.isValidIpv6Block i6b
+isValidIpBlock (IpBlockV4 _)   = True -- TODO
+isValidIpBlock (IpBlockV6 i6b) = I6.isValidIpv6Block i6b
 
 firstAddress :: IpBlock -> (Word32, Word32, Word32, Word32)
-firstAddress (IpBlock (Left i4b))                  = firstAddress (IpBlock (Right $ I6.ipv4BlockToMappedIpv6Block i4b))
-firstAddress (IpBlock (Right (I6.Ipv6Block ip _))) = I6.words ip
+firstAddress (IpBlockV4 i4b)                 = firstAddress (IpBlockV6 (I6.ipv4BlockToMappedIpv6Block i4b))
+firstAddress (IpBlockV6 (I6.Ipv6Block ip _)) = I6.words ip
 
 lastAddress :: IpBlock -> (Word32, Word32, Word32, Word32)
-lastAddress (IpBlock (Left ib)) = (0, 0, 0xFFFF, I4.word (Z.lastIpv4Address ib))
-lastAddress (IpBlock (Right (I6.Ipv6Block ip (I6.Ipv6NetMask msk)))) =
+lastAddress (IpBlockV4 ib) = (0, 0, 0xFFFF, I4.word (Z.lastIpv4Address ib))
+lastAddress (IpBlockV6 (I6.Ipv6Block ip (I6.Ipv6NetMask msk))) =
     let (w1, w2, w3, w4) = I6.words ip
         lt = I6.masksIpv6 $ fromIntegral msk
         w1' = w1 .|. (lt !! 0)
--- test/HaskellWorks/Data/Network/Ipv6Spec.hs
+++ test/HaskellWorks/Data/Network/Ipv6Spec.hs
@@ -17,10 +17,10 @@ spec = describe "HaskellWorks.HUnit.IpSpec" $ do
       show (Ipv6Block (Ipv6Address (3, 3, 3, 0)) (Ipv6NetMask 96)) === "0:3:0:3:0:3::/96"
 
     it "should implement firstAddress/lastAddress" $ require $ property $ do
-      (firstAddress $ IpBlock (Left  $ Ipv4Block (Ipv4Address   0xff000000) (Ipv4NetMask  8)))  === (0, 0, 0xFFFF, 0xFF000000)
-      (firstAddress $ IpBlock (Right $ Ipv6Block (Ipv6Address (4, 4, 0, 0)) (Ipv6NetMask 33)))  === (4, 4, 0, 0)
-      (lastAddress  $ IpBlock (Left  $ Ipv4Block (Ipv4Address   0xff000000) (Ipv4NetMask  8)))  === (0 , 0 , 0xFFFF , 0xFFFFFFFF)
-      (lastAddress  $ IpBlock (Right $ Ipv6Block (Ipv6Address (4, 4, 0, 0)) (Ipv6NetMask 33)))  === (4 , 0x7FFFFFFF , 0xFFFFFFFF , 0xFFFFFFFF)
+      (firstAddress $ IpBlockV4 (Ipv4Block (Ipv4Address   0xff000000) (Ipv4NetMask  8)))  === (0, 0, 0xFFFF, 0xFF000000)
+      (firstAddress $ IpBlockV6 (Ipv6Block (Ipv6Address (4, 4, 0, 0)) (Ipv6NetMask 33)))  === (4, 4, 0, 0)
+      (lastAddress  $ IpBlockV4 (Ipv4Block (Ipv4Address   0xff000000) (Ipv4NetMask  8)))  === (0 , 0 , 0xFFFF , 0xFFFFFFFF)
+      (lastAddress  $ IpBlockV6 (Ipv6Block (Ipv6Address (4, 4, 0, 0)) (Ipv6NetMask 33)))  === (4 , 0x7FFFFFFF , 0xFFFFFFFF , 0xFFFFFFFF)
 
     it "should implement read" $ require $ property $ do
       read "1:2:3:4::/127"  === Ipv6Block (Ipv6Address (0x10002,0x30004,0,0)) (Ipv6NetMask 127)
