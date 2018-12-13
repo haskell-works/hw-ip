0a2c7f4ec767a18bd41a633bf7c9e6ec1d6a80a8
David Turnbull <david@broodax.net>
2018-12-13 14:12:37 +1100

    MAYH-10465 added IPv6 and rearranged while trying to keep the same interface as before (#12)
    * MAYH-10465 added IPv6 and rearranged while trying to keep the same interface as before
    
    * MAYH-10465 sum type
    
    * MAYH-10465 added isValidIpv4Block function
    
    * MAYH-10465 hiding word32x4ToWords
    
    * MAYH-10465 added IpBlock tests
    
    * MAYH-10465 simplified Ipv6NetMask parsing
    
    * MAYH-10465 use appar >= 0.1.7 to get around "adddress" error
    
    * MAYH-10465 minor pointfree
    
    * MAYH-10465 rearranging

--- test/HaskellWorks/Data/Network/IpSpec.hs
+++ test/HaskellWorks/Data/Network/IpSpec.hs
@@ -3,6 +3,7 @@
 module HaskellWorks.Data.Network.IpSpec (spec) where
 
 import HaskellWorks.Data.Network.Ip
+import HaskellWorks.Data.Network.Ip.Ipv4
 import HaskellWorks.Data.Network.Ip.Internal
 import HaskellWorks.Hspec.Hedgehog
 import Hedgehog
@@ -24,23 +25,23 @@ spec = describe "HaskellWorks.HUnit.IpSpec" $ do
       AP.parseOnly octet (T.pack . show $ b) === Right b
 
   describe "Ipv4Address" $ do
-    it "should implement show" $ require $ property $ do
+    it "should implement show" $ requireTest $ do
       show (Ipv4Address 0x000000ff) === "0.0.0.255"
       show (Ipv4Address 0x0000ff00) === "0.0.255.0"
       show (Ipv4Address 0x00ff0000) === "0.255.0.0"
       show (Ipv4Address 0xff000000) === "255.0.0.0"
 
-    it "should implement read" $ require $ property $ do
+    it "should implement read" $ requireTest $ do
       read "1.2.3.4"      === Ipv4Address 0x01020304
       read "10.20.30.40"  === Ipv4Address 0x0a141e28
       read "1.2.3.12"     === Ipv4Address 0x0102030c
       read "1.2.3.160"    === Ipv4Address 0x010203a0
 
-    it "should be possible to extract the octets" $ require $ property $ do
+    it "should be possible to extract the octets" $ requireTest $ do
       ipv4AddressToWords (Ipv4Address 0x01020304) === (1, 2, 3, 4)
 
   describe "Ipv4Block" $ do
-    it "should implement show" $ require $ property $ do
+    it "should implement show" $ requireTest $ do
       show (Ipv4Block (Ipv4Address 0x000000ff) (Ipv4NetMask 32)) === "0.0.0.255/32"
       show (Ipv4Block (Ipv4Address 0x0000ff00) (Ipv4NetMask 32)) === "0.0.255.0/32"
       show (Ipv4Block (Ipv4Address 0x00ff0000) (Ipv4NetMask 32)) === "0.255.0.0/32"
@@ -60,22 +61,22 @@ spec = describe "HaskellWorks.HUnit.IpSpec" $ do
       show (firstIpv4Address $ Ipv4Block (Ipv4Address 0xff000000) (Ipv4NetMask 21)) === "255.0.0.0"
       show (lastIpv4Address  $ Ipv4Block (Ipv4Address 0xff000000) (Ipv4NetMask 21)) === "255.0.7.255"
 
-    it "should implement read" $ require $ property $ do
+    it "should implement read" $ requireTest $ do
       read "1.0.0.0/8"  === Ipv4Block (Ipv4Address 0x01000000) (Ipv4NetMask 8)
       read "1.2.0.0/16" === Ipv4Block (Ipv4Address 0x01020000) (Ipv4NetMask 16)
       read "1.2.3.4/32" === Ipv4Block (Ipv4Address 0x01020304) (Ipv4NetMask 32)
 
-    it "should implement splitBlock" $ require $ property $ do
+    it "should implement splitBlock" $ requireTest $ do
       splitBlock (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask 32)) === Nothing
       splitBlock (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask 31)) === Just (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask 32), Ipv4Block (Ipv4Address 0x00000001) (Ipv4NetMask 32))
       splitBlock (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask 30)) === Just (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask 31), Ipv4Block (Ipv4Address 0x00000002) (Ipv4NetMask 31))
       splitBlock (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask  0)) === Just (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask  1), Ipv4Block (Ipv4Address 0x80000000) (Ipv4NetMask  1))
 
-    it "should implement blockSize" $ require $ property $ do
+    it "should implement blockSize" $ requireTest $ do
       blockSize (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask 32)) === 1
       blockSize (Ipv4Block (Ipv4Address 0x00000000) (Ipv4NetMask  0)) === 0x100000000
 
-    it "should validate masks" $ require $ property $ do
+    it "should validate masks" $ requireTest $ do
       (TR.readMaybe "1.2.3.4/8"  :: Maybe Ipv4Block) === Nothing
       (TR.readMaybe "1.2.3.4/0"  :: Maybe Ipv4Block) === Nothing
       (TR.readMaybe "1.2.3.4/32" :: Maybe Ipv4Block) === (Just $ Ipv4Block (Ipv4Address 0x01020304) (Ipv4NetMask 32))
---
+++ test/HaskellWorks/Data/Network/Ipv6Spec.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module HaskellWorks.Data.Network.Ipv6Spec (spec) where
+
+import HaskellWorks.Data.Network.Ip
+import HaskellWorks.Data.Network.Ip.Ipv4
+import HaskellWorks.Data.Network.Ip.Ipv6
+import HaskellWorks.Hspec.Hedgehog
+import Hedgehog
+import Test.Hspec
+
+import qualified Data.Attoparsec.Text as AP
+import qualified Data.Text            as T
+import qualified Hedgehog.Gen         as G
+import qualified Hedgehog.Range       as R
+import qualified Text.Read            as TR
+
+{-# ANN module ("HLint: ignore Redundant do"  :: String) #-}
+
+spec :: Spec
+spec = describe "HaskellWorks.HUnit.IpSpec" $ do
+  describe "Ipv6Block" $ do
+    it "should implement show" $ requireTest $ do
+      show (Ipv6Block (Ipv6Address (3, 3, 3, 0)) (Ipv6NetMask 96)) === "0:3:0:3:0:3::/96"
+
+    it "should implement firstAddress/lastAddress" $ requireTest $ do
+      (firstAddress $ IpBlockV4 (Ipv4Block (Ipv4Address   0xff000000) (Ipv4NetMask  8)))  === (0, 0, 0xFFFF, 0xFF000000)
+      (firstAddress $ IpBlockV6 (Ipv6Block (Ipv6Address (4, 4, 0, 0)) (Ipv6NetMask 33)))  === (4, 4, 0, 0)
+      (lastAddress  $ IpBlockV4 (Ipv4Block (Ipv4Address   0xff000000) (Ipv4NetMask  8)))  === (0 , 0 , 0xFFFF , 0xFFFFFFFF)
+      (lastAddress  $ IpBlockV6 (Ipv6Block (Ipv6Address (4, 4, 0, 0)) (Ipv6NetMask 33)))  === (4 , 0x7FFFFFFF , 0xFFFFFFFF , 0xFFFFFFFF)
+
+    it "should implement read" $ requireTest $ do
+      read "1:2:3:4::"      === Ipv6Address (0x10002,0x30004,0,0)
+      read "1:2:3:4::/127"  === Ipv6Block (Ipv6Address (0x10002,0x30004,0,0)) (Ipv6NetMask 127)
+      read "1234::/16"      === Ipv6Block (Ipv6Address (0x12340000, 0, 0, 0)) (Ipv6NetMask 16)
+      read "12:34::/32"     === Ipv6Block (Ipv6Address (0x120034, 0, 0, 0))   (Ipv6NetMask 32)
+
+    it "should parse what it has shown" $ require $ property $ do
+      a <- forAll $ G.word32 R.constantBounded
+      m <- forAll $ G.word8 $ R.linear 0 128
+      let addr = Ipv6Block (Ipv6Address (a, 0, 0, 0)) (Ipv6NetMask 1)
+      parseIpv6Block (T.pack (show addr)) === Right addr
