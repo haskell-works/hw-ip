8f867e78518f35e900806b6131ab4bf2fa24fe1f
John Ky <newhoggy@gmail.com>
2018-12-18 12:07:25 +1100

    Using safe enum

--- src/HaskellWorks/Data/Network/Ip/SafeEnum.hs
+++ src/HaskellWorks/Data/Network/Ip/SafeEnum.hs
@@ -1,17 +1,73 @@
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE UndecidableInstances #-}
+module HaskellWorks.Data.Network.Ip.SafeEnum
+  ( SafeEnum(..)
+  , boundedPred
+  , boundedSucc
+  ) where
 
-module HaskellWorks.Data.Network.Ip.SafeEnum where
+import Data.Int
+import Data.Maybe
+import Data.Word
 
 class SafeEnum a where
   safePred :: a -> Maybe a
   safeSucc :: a -> Maybe a
 
-  unsafePred :: a -> a
-  unsafeSucc :: a -> a
+instance SafeEnum Word where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
 
-instance (Bounded a, Enum a, Eq a) => SafeEnum a where
-  safePred v = if v /= minBound then Just (pred v) else Nothing
-  safeSucc v = if v /= maxBound then Just (succ v) else Nothing
-  unsafePred = pred
-  unsafeSucc = succ
+instance SafeEnum Word8 where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Word16 where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Word32 where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Word64 where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Int where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Int8 where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Int16 where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Int32 where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Int64 where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Char where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+instance SafeEnum Bool where
+  safePred = defaultSafePred
+  safeSucc = defaultSafeSucc
+
+boundedPred :: SafeEnum a => a -> a
+boundedPred a = fromMaybe a (safePred a)
+
+boundedSucc :: SafeEnum a => a -> a
+boundedSucc a = fromMaybe a (safeSucc a)
+
+defaultSafePred :: (Bounded a, Enum a, Eq a) => a -> Maybe a
+defaultSafePred v = if v /= minBound then Just (pred v) else Nothing
+
+defaultSafeSucc :: (Bounded a, Enum a, Eq a) => a -> Maybe a
+defaultSafeSucc v = if v /= minBound then Just (succ v) else Nothing
